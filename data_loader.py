55c97988eb0060c4f539a7e4c33119a7ff9735fa
Fan Yang <128557286+yang200347@users.noreply.github.com>
2026-04-16 18:38:41 -0400

    Add files via upload
    This module fetches daily OHLCV data for SPY using Alpha Vantage API and converts it into a structured pandas DataFrame

---
+++ data_loader.py
@@ -0,0 +1,31 @@
+import requests
+import pandas as pd
+import os
+
+API_KEY = os.getenv("ALPHA_API_KEY")
+
+url = "https://www.alphavantage.co/query"
+params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": "SPY",
+    "apikey": API_KEY
+}
+
+res = requests.get(url, params=params)
+data = res.json()
+
+# error handling
+if "Time Series (Daily)" not in data:
+    print("API error:", data)
+    exit()
+
+ts = data["Time Series (Daily)"]
+
+df = pd.DataFrame(ts).T
+df.index = pd.to_datetime(df.index)
+df = df.sort_index()
+
+df.columns = ["open", "high", "low", "close", "volume"]
+df = df.astype(float)
+
+print(df.head())
